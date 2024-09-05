f7044fc200d8dd7b39b023a543f55902960a7109
somaia <Som@ia>
2024-09-05 20:14:33 +0100

    up

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -6,7 +6,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 const initialState={}
 const middleware=[thunk]
 // we applay thunl to solve async await problem and Devtools to show store during process
-// const store =createStore(rootReducer,initialState,applyMiddleware(...middleware))
+const store =createStore(rootReducer,initialState,applyMiddleware(...middleware))
 
-const store =createStore(rootReducer,initialState,composeWithDevTools(applyMiddleware(...middleware)))
+// const store =createStore(rootReducer,initialState,composeWithDevTools(applyMiddleware(...middleware)))
 export default store;
